5d4c6c65e407a802553b2af9b755a15fb3631abb
Andrey Baranov <tread_lightly@list.ru>
2023-07-19 13:31:57 +1000

    3. Каркас шахматной доски (edited BishopBlackTest)

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,10 +1,12 @@
 package ru.job4j.chess.firuges.black;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
 class BishopBlackTest {
 
@@ -18,7 +20,7 @@ class BishopBlackTest {
     @Test
     void whenWayThenArrayOfDiagonalCells() {
         Figure bishop = new BishopBlack(Cell.C1);
-        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5,};
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
         assertThat(expected).containsExactly(bishop.way(Cell.G5));
     }
 
@@ -33,5 +35,16 @@ class BishopBlackTest {
         Cell destPosition = Cell.findBy(1, 3);
         Figure srcFigure = new BishopBlack(Cell.findBy(1, 1));
         assertThat(Cell.B5).isEqualTo(srcFigure.copy(destPosition).position());
+    }
+
+    @Test
+    void whenIncorrectWayThenThrowImpossibleMoveException() throws ImpossibleMoveException {
+        Cell source = Cell.C1;
+        Cell dest = Cell.A1;
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException   .class, () -> {
+            new BishopBlack(Cell.C1).way(Cell.A1);
+        });
+        assertThat(exception.getMessage()).isEqualTo(String.format(
+                "Could not move by diagonal from %s to %s", source, dest));
     }
 }
